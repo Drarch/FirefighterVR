808e3e3e4f147d0e9a7a901f2adf0fad3faa2c4b
Tomasz Szymański <cl.arctos.orion@gmail.com>
2018-01-21 18:59:03 +0100

    Dodanie testowegopomieszczenia z oświetleniem.

---
+++ Assets/Scripts/VRMouseCameraInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRMouseCameraInput : MonoBehaviour {
+
+    private Vector3 mousePosStart;
+    public float speed = 0.75f;
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetButtonDown("Jump"))
+        {
+            mousePosStart = Input.mousePosition;
+        }
+
+		if(Input.GetButton("Jump"))
+        {
+            Vector3 rotation = new Vector3(mousePosStart.y - Input.mousePosition.y, Input.mousePosition.x - mousePosStart.x, 0) * speed;
+
+            transform.Rotate(rotation);
+
+            mousePosStart = Input.mousePosition;
+        }
+	}
+}
