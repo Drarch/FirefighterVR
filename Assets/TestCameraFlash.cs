049a2102a15db5e73bb9e22e0a6b9d67bc8fe7c7
Tomasz Szymanski <cl.arctos.orion@gmail.com>
2018-05-17 09:18:27 +0200

    Add: Flash camera - electric zap Add: Sounds electric

---
+++ Assets/TestCameraFlash.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class TestCameraFlash : MonoBehaviour {
+
+    public VRTK_HeadsetFade fade;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(FlashCamera());
+        }
+	}
+
+    IEnumerator FlashCamera()
+    {
+        fade.Fade(Color.white, 0.01f);
+        yield return new WaitForSeconds(2);
+        fade.Unfade(2);
+    }
+}
