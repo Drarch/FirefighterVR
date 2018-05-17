792f41f4e414742abd985a0d26413f5b905e4582
Tomasz Szymanski <cl.arctos.orion@gmail.com>
2018-05-17 13:45:38 +0200

    Fix: Temporary fix item dash grab Add: camera electric shock effect

---
+++ Assets/ElectricSchock.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ElectricSchock : MonoBehaviour {
+
+    public VRTK_HeadsetFade fade;
+    public Transform respawn;
+
+    public VRTK_DashTeleport dashTeleport;
+    
+    // Use this for initialization
+    void Start ()
+    {
+            //dashTeleport.WillDashThruObjects += new DashTeleportEventHandler(DashThru);
+            //dashTeleport.DashedThruObjects += new DashTeleportEventHandler(RendererOn);
+
+            //Debug.LogFormat("Teleport: {0}, {1}", dashTeleport, this.name);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void DashThru(object sender, DashTeleportEventArgs e)
+    {
+        //StartCoroutine(FlashCamera());
+        Debug.LogFormat("Flash DashThru: {0}", this.name);
+    }
+
+    IEnumerator FlashCamera()
+    {
+        Debug.LogFormat("Fade: {0}", this.name);
+
+        fade.Fade(Color.white, 0.01f);
+        
+        yield return new WaitForSeconds(2);
+        fade.Unfade(2);
+    }
+
+    void Flash()
+    {
+        fade.Fade(Color.white, 0.01f);
+        //fade.Unfade(4);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!fade.IsFaded())
+        {
+            StartCoroutine(FlashCamera());
+            //Flash();
+        }
+        Debug.LogFormat("Flash Trigger: {0}", this.name);
+    }
+}
---
+++ Assets/Scripts/TestCameraFlash.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class TestCameraFlash : MonoBehaviour {
+
+    public VRTK_HeadsetFade fade;
+
+    private VRTK_ControllerReference controllerReference;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameObject rightHand = VRTK_DeviceFinder.GetControllerRightHand(true);
+        controllerReference = VRTK_ControllerReference.GetControllerReference(rightHand);
+        
+        if (IsPressed() || Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(FlashCamera());
+        }
+	}
+
+    private bool IsPressed()
+    {
+        if (!VRTK_ControllerReference.IsValid(controllerReference))
+        {
+            return false;
+        }
+
+        if(VRTK_SDK_Bridge.GetControllerButtonState(SDK_BaseController.ButtonTypes.ButtonTwo, SDK_BaseController.ButtonPressTypes.Press, controllerReference))
+        {
+            Debug.Log("VR flash");
+            return true;
+        }
+
+        return false;
+    }
+
+    IEnumerator FlashCamera()
+    {
+        fade.Fade(Color.white, 0.01f);
+        yield return new WaitForSeconds(2);
+        fade.Unfade(2);
+    }
+}
